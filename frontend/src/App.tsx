873fb0582fedfc5fd2ef04d05ad46db5636d1d74
Daniel <danielzhuzhang@gmail.com>
2025-06-21 21:07:43 -0400

    Simple prompt engineering example, parsing draw.io file and explaining logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UploadImg from '../routes/UploadImg.tsx';
 import SendToGem from "../components/SendToGem.tsx"
+import SendXMLToGem from "../components/SendXMLToGem.tsx"
 
 const App = () => {
     return(
@@ -8,6 +9,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<UploadImg />} />
                 <Route path="/gem" element={<SendToGem />} />
+                <Route path="/XMLgem" element={<SendXMLToGem />} />
             </Routes>
         </Router>
     );
