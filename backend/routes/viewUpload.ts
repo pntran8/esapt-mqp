68464fe077ee3126d93014afe2addc388af106d5
Phuong Tran <ptran.nup@gmail.com>
2025-08-02 15:13:17 -0400

    changed from ../../.prisma/client to import { PrismaClient } from @prisma/client in viewUpload

--- backend/routes/viewUpload.ts
+++ backend/routes/viewUpload.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../.prisma/client";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 import express, {Request, Response, Router} from "express";
 //import zlib from "zlib";
