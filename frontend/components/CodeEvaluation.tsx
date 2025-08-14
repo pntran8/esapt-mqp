e9a166911594593856ca9f54f2d9672a3847b008
JackBurke164 <jjburke@wpi.edu>
2025-08-14 16:47:16 -0400

    Loading animations

--- frontend/components/CodeEvaluation.tsx
+++ frontend/components/CodeEvaluation.tsx
@@ -14,7 +14,8 @@ import {useNavigate} from "react-router-dom";
 import * as React from "react";
 import {useAuth0} from "@auth0/auth0-react";
 import StartSessionBtn from "./StartSessionBtn.tsx";
-import download from "@/src/assets/download.png";
+import download from "../src/assets/download.png";
+import { PulseLoader } from "react-spinners";
 
 // Message type definition
 interface Message {
@@ -211,7 +212,9 @@ const CodeEvaluation = () => {
             <div style={{height:'75vh'}}>
                 <div className={"inner-page-box w-[35vw] h-[70vh] float-left overflow-scroll"} style={{marginLeft:"20vh"}}>
                     {response.length === 0 ? (
-                        <h1 style={{fontSize:'2.5vh'}}>Upload your image to see code</h1>
+                        isLoading ? (<div><h1 style={{fontSize:'max(15px, 2.5vh)'}}>Loading SQL, please wait...</h1>
+                                <PulseLoader color={"black"} loading={isLoading} size={15} margin={4} aria-label="Loading Spinner" data-testid="loader"/></div>)
+                            : (<h1 style={{fontSize:'max(15px, 2.5vh)'}}>Upload your image to see code</h1>)
                     ) : (
                         <div>
                             {response.map((msg, index) => (
