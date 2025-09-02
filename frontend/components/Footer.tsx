4abb8a8f9dd599c85a0087d290f747dcdf697aac
pdurgin311 <pdurgin311@gmail.com>
2025-09-01 20:40:59 -0400

    misc fixes

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -7,7 +7,8 @@ export default function Footer() {
     }
     return (
         <>
-            <div className={"h-[8vh] m-[1vh] mt-[4vh]"} style={{textAlign: "left"}}>
+            <footer
+                className="sticky top-[100vh] bottom-0 w-full mx-auto p-1 mt-[5vh] bg-[#E7E7E7] flex items-center justify-center h-20">
                 <button
                     className={'circle-button'}
                     style={{width: "3vh", height: "3vh"}}
@@ -16,9 +17,6 @@ export default function Footer() {
                     }}>
                     <h2 style={{color: "white", fontSize: "2vh"}}>i</h2>
                 </button>
-            </div>
-            <footer
-                className="sticky top-[100vh] bottom-0 w-full mx-auto p-1 mt-1 bg-[#E7E7E7] flex items-center justify-center h-20">
                 <h2 className={"text-black"}>This website was created as an MQP advised by professors Rodica Neamtu and
                     Wilson Wong. Any inquiries can be directed to gr-esapt-mqp-2025@wpi.edu</h2>
             </footer>
